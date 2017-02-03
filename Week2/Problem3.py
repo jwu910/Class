d4d13a6b63456ead1dd72cfe197e61be9bb24408
Joshua <jwu910@yahoo.com>
2017-02-03 07:08:51 -0800

    completed Problem 3

--- Week2/Problem3.py
+++ Week2/Problem3.py
@@ -14,12 +14,42 @@ annualInterestRate = 0.2
 
 remBal = balance # using Test Case
 monInt = annualInterestRate / 12 
-maxIter = 15
+maxIter = 30
 monLow = remBal / 12
-monHigh = (remBal * ((1 + monInt)**12)) / 12.0
+monHigh = (balance * ((1 + monInt)**12)) / 12.0
 monMid = (monLow + monHigh) / 2.0
 TOL = .01
 counter = 0
 
-print(remBal, monLow, monMid, monHigh, TOL)
+while counter < maxIter:
+    monMid = (monLow + monHigh)/2.0
+    for i in range(12):
+        remBal -= monMid
+        remBal = remBal + (remBal * monInt)
+    if remBal == 0 or (monLow+monHigh)/2 < TOL:
+        print(monMid)
+        break
+    if remBal > 0:
+        monLow = monMid
+        remBal = balance
+    else:
+        monHigh = monMid
+        remBal = balance
+        
+    counter += 1
+    #print('not found, checking again',counter)
+    
+print(round(monMid,2)) 
+    
+    
 
+
+"""
+while preBal > 0:
+    minFixPay += 10
+    preBal = balance
+    for month in range(0,12):
+        monUnPaid = preBal - minFixPay # Monthly unpaid balance
+        preBal = monUnPaid + (monUnPaid * monInt) # update balance
+print(minFixPay)
+"""
